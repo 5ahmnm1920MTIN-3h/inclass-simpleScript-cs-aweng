4af160ccdf4c4de3af3e9c09c1e969da389f1a2e
aweng <annawenger@gmx.at>
2019-12-10 16:52:07 +0100

    code rewritten

--- Assets/MyGame/Scripts/CommputeNumbers.cs
+++ Assets/MyGame/Scripts/CommputeNumbers.cs
@@ -3,48 +3,32 @@ using UnityEngine.UI;
 
 public class CommputeNumbers : MonoBehaviour
 {
-    public InputField ipVarA;
-    public InputField ipVarB;
-    public Text result;
+    public InputField input1;
+    public InputField input2;
+
+    public Text resultLabel;
 
     float varA;
     float varB;
 
     public void SetResult()
     {
-        float a = float.Parse(ipVarA.text);
-        float b = float.Parse(ipVarB.text);
-        Debug.Log("in SetResult" + (a - b));
-        //result.text = SubstractNumbers(a, b);
-        float resultAB = a - b;
-        result.text = resultAB.ToString();
-
+        resultLabel.text = SubstractNumbers();
     }
 
-    public string TestMethode()
+    string SubstractNumbers()
     {
-        Debug.Log("Test");
-        return "Test";
+        varA = float.Parse(input1.text);
+        varB = float.Parse(input2.text);
+
+        string outputSuccess = (varA - varB).ToString();
+        return outputSuccess;
     }
-    public void TestMethodeVOID()
+
+    public void ResetApp()
     {
-        Debug.Log("Test");
-    }
-    public float MultNumbers(float variableA, float variableB)
-    {
-        float result = variableA * variableB;
-        //Debug.Log("Test sub" + result.ToString());
-        return result;
-    }
-    public float SubstractNumbers(float variableA, float variableB)
-    {
-        float result = variableA - variableB;
-        //Debug.Log("Test sub" + result.ToString());
-        return result;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+        input1.text = "";
+        input2.text = "";
+        resultLabel.text = "Result";
     }
 }
